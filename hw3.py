6ff258694962cff17df16dbd83b023c7958a5ed3
Charlie Barge <artemisc360@gmail.com>
2016-08-03 21:19:39 -0400

    more prints

--- hw3.py
+++ hw3.py
@@ -5,9 +5,16 @@ from sympy import *
 
 m_example = Matrix([[2, 4, 2], [5, 1, 3], [8, 6, 3]])
 
+print("Example matrix: \n")
+print(m_example)
+
 def get_determinant(m):
 	num_cols = m.shape[0] # extract number of columns
 	det_list = [] # list for use later on
+
+	# exit if too big
+	if (num_cols > 10):
+		return "Matrix too big"
 
 	# if 2x2 then calculate determinant easily, otherwise recursive call
 	if (num_cols == 2): 
@@ -28,10 +35,12 @@ def get_determinant(m):
 
 	return sum(det_list) # return final result
 
+print("\nExample matrix determinant: ")
 print(get_determinant(m_example))
 
-
+print("\nExample triangle points: \n")
 points_example = [(3, 4), (1, 7), (2, 1)] # example data
+print(points_example)
 
 # takes a list of tuples
 def area_of_triangle(points):
@@ -46,4 +55,15 @@ def area_of_triangle(points):
 
 	return .5 * get_determinant(triangle_matrix) # compute result
 
+print("\nExample triangle area, calculated with determinant method: ")
 print(area_of_triangle(points_example))
+
+problem11matrix = Matrix([[4,3,2],[1,2,6],[5,8,1]])
+
+print("\nEigenvalues for problem 11 matrix: \n")
+print(problem11matrix.eigenvals())
+
+print("\nEigenvectors for problem 11 matrix: \n")
+print(problem11matrix.eigenvects())
+
+
